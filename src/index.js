c5ae594c651236bc3e153f5e7142c37c119a91f2
nrednav <nrednav@gmail.com>
2021-01-22 00:14:43 +0400

    Add Orbit Controls, Canvas resizing logic
    + Set the renderer's device pixel ratio
    + Add camera near & far plane
    + Update animation logic to use THREE.Clock()
    + Add ambient lighting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 // Config
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 // Canvas & Renderer
@@ -13,6 +14,22 @@ const renderer = new THREE.WebGLRenderer({
   antialias: true,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// Handle window resizing
+window.addEventListener('resize', () => {
+  // Update dimensions
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
 
 // Scene
 const scene = new THREE.Scene();
@@ -22,24 +39,39 @@ const light = new THREE.DirectionalLight('#ffffff');
 light.position.set(0, 5, 5);
 scene.add(light);
 
+const ambientLight = new THREE.AmbientLight('#ffffff', 0.25);
+scene.add(ambientLight);
+
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  1000
+);
+camera.position.set(0, 0, 3);
 scene.add(camera);
+
+// Orbit Controls
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+orbitControls.enableDamping = true;
 
 // Object
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshPhongMaterial({ color: 'red' });
+const material = new THREE.MeshPhongMaterial({ color: 'skyblue' });
 const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
-// Render
+// Render & Animation
+const clock = new THREE.Clock();
 const render = () => {
   requestAnimationFrame(render);
 
-  mesh.rotation.x += 0.01;
-  mesh.rotation.y += 0.01;
+  // Animation logic
+  const elapsedTime = clock.getElapsedTime();
+  mesh.rotation.y = elapsedTime;
 
+  orbitControls.update();
   renderer.render(scene, camera);
 };
 
